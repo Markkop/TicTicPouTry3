366c5203856f8e1565d3f47d67a2260cbe7a5912
Mark Kop <mk.marcelo.mk@gmail.com>
2018-11-23 12:05:47 -0200

    Initial commit

---
+++ Assets/Atributos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atributos : MonoBehaviour {
+
+	public int playerId;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_Manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Manager : MonoBehaviour {
+
+	public GameObject[] gameObjectArray;
+	public List<GameObject> gameObjectList = new List<GameObject>();
+	public List<int> intList = new List<int>();
+
+	public GameObject player1Text;
+	public GameObject player2Text;
+	public GameObject player3Text;
+
+	void Start () {
+
+		listaPlayers();
+		
+	}
+	
+	void Update () {
+
+		listaPlayers();
+		
+	}
+
+	void listaPlayers()
+	{
+		gameObjectArray = GameObject.FindGameObjectsWithTag("Player"); //Popula o array com GameObjects de players
+		//gameObjectList = gameObjectArray.ToList(); // Transforma o array em list
+
+		foreach (GameObject go in gameObjectArray) // Para cada objeto no array
+		{
+			Debug.Log("Tamanho do array gameObjectArray: "+gameObjectArray.Length);
+			for(int i = 0; i < gameObjectArray.Length; i++)
+	        {
+        		go.GetComponent<Atributos>().playerId = i; // assigna o playerId
+    		}
+	}	}
+}
